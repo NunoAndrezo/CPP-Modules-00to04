70b65b933bdd598804755c3b76989e7ef2287566
nuno <nunoandrezo97@gmail.com>
2025-07-08 17:32:05 +0100

    on the making

---
+++ ex01/src/main.cpp
@@ -0,0 +1,22 @@
+#include "phonebook.hpp"
+#include "contact.hpp"
+
+int	main(void)
+{
+	Phonebook	phonebook;
+	bool		running = true;
+
+	while (running)
+	{
+		else if ()
+		{
+			running = 0;
+			continue;
+		}
+	}
+	if (running)
+	{
+		//fazer para o ctrl D
+	}
+	return (0);
+}
