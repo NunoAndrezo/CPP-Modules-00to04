b087d63308a2ce53be0aff5520a53f36d1d97159
nuno <nuno@ubuntu-vm.ubuntu-vm.virtualbox.org>
2025-09-06 10:26:55 +0100

    mod01 ex04 - Done

--- mod01/ex04/src/files_handler.cpp
+++ mod01/ex04/src/files_handler.cpp
@@ -1,6 +1,6 @@
 #include "../inc/files_handler.hpp"
 
-static void	copying_and_correcting(std::ifstream *input, std::ofstream *output);
+static void	copying_and_correcting(std::ifstream *input, std::ofstream *output, Helper *helper);
 
 void	handle_files(char **av, Helper *helper)
 {
@@ -9,30 +9,37 @@ void	handle_files(char **av, Helper *helper)
 	std::ifstream input(av[1], std::ios_base::binary); // av[1] == file que vamos abrir
 	if (!input.is_open())
 	{
-		std::cerr << "Error:" << av[1] << "does not exist\n";
+		std::cerr << "Error: an \"" << av[1] << "\" file does not exist\n";
 		exit(1);
 	}
-	std::ofstream output("copy.txt", std::ios::binary);
+	std::ofstream output("copy", std::ios::binary);
 	if (!output.is_open())
 	{
-		std::cerr << "Error: Could not create copy.txt\n";
+		std::cerr << "Error: Could not create a copy\n";
 		exit(1);
 	}
 	if (!input || !output)
 	{
-		std::cerr << "Error opening files\n";
+		std::cerr << "Error opening or creating files\n";
 		exit(1);
 	}
-	copying_and_correcting(&input, &output);
-	(void)helper;
+	copying_and_correcting(&input, &output, helper);
 }
 
-static void	copying_and_correcting(std::ifstream *input, std::ofstream *output)
+static void	copying_and_correcting(std::ifstream *input, std::ofstream *output, Helper *helper)
 {
-	char	c;
+	std::string line;
+	size_t position;
 
-	while ((*input).get(c))
+	while (std::getline((*input), line))
 	{
-		(*output).put(c);
+		position = 0;
+		while ((position = line.find(helper->erase, position)) != std::string::npos)
+		{
+			line.erase(position, helper->erase.length());
+			line.insert(position, helper->replace);
+			position += helper->replace.length();
+		}
+		(*output) << line << std::endl;
 	}
 }
--- mod01/ex04/src/parser.cpp
+++ mod01/ex04/src/parser.cpp
@@ -7,7 +7,7 @@ bool	parser(int ac, char **av, Helper *helper)
 	if (!av[2] || !av[3])
 		return (std::cout << " Something is wrong, use the Correct example: ./<exec> <filename> <string to be replaced> <string to replace>" << std::endl, false);
 	else if (!av[3][0] || !av[2][0] )
-		return (std::cout << "Nothing will be replaced arguments is/are empty: '<"">', :( exiting...\n", false);
+		return (std::cout << "Nothing will be replaced, since the argument/s is/are empty: <"">, exiting...\n", false);
 	helper->erase = av[2];
 	helper->replace = av[3];
 	return (true);
