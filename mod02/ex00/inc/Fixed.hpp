632920ef330c42a6fb3c9ae17ff19c59d68cbabf
nuno <nuno@ubuntu-vm.ubuntu-vm.virtualbox.org>
2025-09-15 18:44:49 +0100

    mod02 ex00 - Done

--- mod02/ex00/inc/Fixed.hpp
+++ mod02/ex00/inc/Fixed.hpp
@@ -1,6 +1,8 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+#include <string>
 class Fixed
 {
 	private:
@@ -8,12 +10,11 @@ class Fixed
 		static const int	nr_fractionl_bits;
 
 	public:
-		Fixed();
-		Fixed(const Fixed &other);
-		Fixed &operator = (const Fixed &other);
-		~Fixed();
-
-		int getRawBits(void) const;
+		Fixed(); // constructor
+		Fixed(const Fixed &other); //copy constructor
+		Fixed &operator = (const Fixed &other); // copy assignment operator overload
+		~Fixed(); // destructor
+		int getRawBits(void) const; // const after the parentheses promises that the function will not modify the object
 		void setRawBits(int const raw);
 };
 
---
+++ mod02/ex00/src/Fixed.cpp
@@ -0,0 +1,57 @@
+#include "../inc/Fixed.hpp"
+
+Fixed::Fixed() : nr_fixed_point(0)
+{
+	std::cout << "Default constructor called \n";
+}
+
+Fixed::Fixed(const Fixed &other) : nr_fixed_point(other.nr_fixed_point)
+{
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed &Fixed::operator = (const Fixed &other)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this != &other) // protect against self-assignment
+		nr_fixed_point = other.nr_fixed_point;
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int	Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return (this->nr_fixed_point);
+}
+
+void	Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called\n";
+	this->nr_fixed_point = raw;
+}
+
+/*
+$> ./a.out
+Default constructor called
+Copy constructor called
+Copy assignment operator called // <-- This line may be missing depending on your implementation
+getRawBits member function called
+Default constructor called
+Copy assignment operator called
+getRawBits member function called
+getRawBits member function called
+0
+getRawBits member function called
+0
+getRawBits member function called
+0
+Destructor called
+Destructor called
+Destructor called
+$>
+ */
